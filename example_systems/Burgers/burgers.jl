9738a93a40e22007af6ef618eac5b2b594b95c35
jakelowellmurphy <murphy.jake144@gmail.com>
2024-06-01 19:13:16 -0600

    Added functionality for semicirc2

--- example_systems/Burgers/burgers.jl
+++ example_systems/Burgers/burgers.jl
@@ -1,5 +1,6 @@
 include("../../wavelab/structs.jl")
 include("../../wavelab/emcset.jl")
+include("../../wavelab/semicirc2.jl")
 
 # TODO::Is there a Beep off equivalent in Julia? Do we care enough to do it?
 
@@ -18,3 +19,14 @@ s = Infin(12, 12, -12, nothing, nothing)
 s, e, m, c = emcset(s, "front", [1, 1], "default")   # default for Burgers is reg_reg_polar
 
 # preimage contour
+
+circpnts = 20
+imagpnts = 20
+innerpnts = 5
+r = 10
+spread = 4
+zerodist = 1e-2
+
+preimage = semicirc2(circpnts, imagpnts, innerpnts, c.ksteps, r, spread, zerodist, c.lambda_steps)
+
+
